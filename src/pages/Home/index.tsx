0549e6508252b4989e0dbd21fd026a90e5dea272
Liam Ozog <liam.ozog@gmail.com>
2022-02-12 15:04:58 -0500

    add styles for Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,38 +4,36 @@ import * as S from "./styles";
 
 export function Home() {
   return (
-    <div className="Home">
-      <div className="Home__container">
-        <div className="Home__centerpiece">
-          <Link to={"/music"}>
-            <div className="Home__image Home__image--1">
-              <img src="https://f4.bcbits.com/img/a0642444381_16.jpg" alt="music" />
-              <div className="Home__image-overlay">
-                <div className="Home__image-overlay-text">Music</div>
-              </div>
-            </div>
-          </Link>
-          <Link to={"/about"}>
-            <div className="Home__image Home__image--2">
-              <picture>
-                <img src={"https://res.cloudinary.com/dtxbzcy7f/image/upload/v1627956234/personal-site/000017960026-26_bbaaro.jpg"} alt="me" />
-              </picture>
-              <div className="Home__image-overlay">
-                <div className="Home__image-overlay-text">About</div>
-              </div>
-            </div>
-          </Link>
-          <Link to={"/photography"}>
-            <div className="Home__image Home__image--3">
-              <img src="https://res.cloudinary.com/dtxbzcy7f/image/upload/v1627956431/personal-site/000039-68_pn2jdm.jpg" alt="photography" />
-              <div className="Home__image-overlay">
-                <div className="Home__image-overlay-text">Photography</div>
-              </div>
-            </div>
-          </Link>
-          <div className="Home__clear"></div>
-        </div>
-      </div>
-    </div>
+    <S.Container>
+      <S.Centerpiece>
+        <Link to={"/music"}>
+          <S.Image1>
+            <img src="https://f4.bcbits.com/img/a0642444381_16.jpg" alt="music" />
+            <S.ImageOverlay>
+              <S.ImageOverlayText>Music</S.ImageOverlayText>
+            </S.ImageOverlay>
+        </S.Image1>
+        </Link>
+        <Link to={"/about"}>
+          <S.Image2>
+            <picture>
+              <img src={"https://res.cloudinary.com/dtxbzcy7f/image/upload/v1627956234/personal-site/000017960026-26_bbaaro.jpg"} alt="me" />
+            </picture>
+            <S.ImageOverlay>
+              <S.ImageOverlayText>About</S.ImageOverlayText>
+            </S.ImageOverlay>
+          </S.Image2>
+        </Link>
+        <Link to={"/photography"}>
+          <S.Image3>
+            <img src="https://res.cloudinary.com/dtxbzcy7f/image/upload/v1627956431/personal-site/000039-68_pn2jdm.jpg" alt="photography" />
+            <S.ImageOverlay>
+              <S.ImageOverlayText>Photography</S.ImageOverlayText>
+            </S.ImageOverlay>
+          </S.Image3>
+        </Link>
+        <S.ClearFloat />
+      </S.Centerpiece>
+    </S.Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,101 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  margin: 75px auto 0;
+  width: 100%;
+`;
+
+export const Centerpiece = styled.div`
+  width: 808px;
+  margin: 0 auto;
+`;
+
+export const ImageOverlay = styled.div`
+  position: absolute;
+  display: grid;
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  opacity: 0;
+  transition: .3s ease;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const ImageOverlayText = styled.div`
+  color: $color-white;
+  font-weight: 600;
+  font-size: 24pt;
+  text-transform: uppercase;
+  margin: auto;
+  letter-spacing: 2px;
+`;
+
+const Image = styled.div`
+  position: relative;
+  cursor: pointer;
+  background-color: black;
+  background-position: center;
+  background-size: cover;
+  float: left;
+
+  /*
+                +-----------+
+                |           |
+                |           |
+  +----------+  |           |
+  |          |  |           |
+  |          |  |           |
+  |     1    |  |           |
+  |          |  |     2     |
+  |          |  |           |
+  +----------+  |           |
+     +-------+  |           |
+     |       |  |           |
+     |   3   |  +-----------+
+     |       |
+     |       |
+     +-------+
+  */
+
+  img {
+    display: block;
+  }
+`;
+
+export const Image1 = styled(Image)`
+  margin-top: 40px;
+
+  img {
+    width: 431px;
+    height: 308px;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+
+export const Image2 = styled(Image)`
+  margin-left: 20px;
+
+  img {
+    width: 357px;
+    height: 500px;
+  }
+`;
+
+export const Image3 = styled(Image)`
+  transform: translateY(-131px);
+  margin-left: 61px;
+
+  img {
+    width: 370px;
+    height: 240px;
+  }
+`;
+
+export const ClearFloat = styled.div`
+  clear: both;
 `;
