62b9e549bab237c3c47a25b8527d0547558fa14e
Liam Ozog <liam.ozog@gmail.com>
2022-02-13 18:33:24 -0500

    install @types/styled-components

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { colours, dims } from "globalStyle";
 
 export const Container = styled.div`
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { dims } from "globalStyle";
 
 export const Container = styled.div`
